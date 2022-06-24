59dfa2ecd20779116fda35bddd0bac2c2a831464
Balázs Simon <balazs.simon@intren.hu>
2022-06-24 16:26:22 +0200

    [fix] rename firith_imagine to imagine_preset

--- src/helpers.php
+++ src/helpers.php
@@ -1,7 +1,7 @@
 <?php
 
-if (!function_exists('imagine_use_preset')) {
-    function imagine_use_preset($path, $preset) {
+if (!function_exists('imagine_preset')) {
+    function imagine_preset($path, $preset) {
         return \Firith\Imagine\Utility\ImagineUtility::url($path, $preset);
     }
 }
